02b673aecd49bc2dc41cd50c834c1ae278debb5d
MaryiaChuhunova <Maryia_Chuhunova@epam.com>
2017-02-05 21:52:27 +0400

    Create ReleaseAdditionDAOTest.java

---
+++ src/test/java/DAO/ReleaseAdditionDAOTest.java
@@ -0,0 +1,16 @@
+package DAO;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by Maria on 05.02.2017.
+ */
+public class ReleaseAdditionDAOTest {
+    @Test
+    public void testAddRelease() throws Exception {
+
+    }
+
+}
